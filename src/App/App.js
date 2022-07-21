958911138486d053bd33b6263ee4f8505b539af7
leonardo0920 <leo.davila0920@gmail.com>
2022-07-21 15:06:51 -0400

    Cambios

--- src/App/App.js
+++ src/App/App.js
@@ -1,40 +1,50 @@
 import React from 'react';
 import { TodoCounter } from "../TodoCounter";
+import { TodoContext } from "../TodoContext";
 import { TodoSearch} from "../TodoSearch";
 import { TodoList } from "../TodoList";
 import { TodoItem } from "../TodoItem";
 import { CreateTodoButton } from "../CreateTodoButton";
+import { Modal } from "../Modal";
 
-function AppUI({
-    totalTodos,
-    completedTodos,
-    searchValue,
-    setSearchValue,
-    searchedTodos,
-    completeTodo,
-    deleteTodo,
-}) {
+function AppUI() {
+    const {
+        error,
+        loading,
+        searchedTodos,
+        completeTodo,
+        deleteTodo,
+        openModal,
+        setOpenModal,
+    } = React.useContext(TodoContext);
+
     return (
-        <React.Fragment>
-        <TodoCounter
-        total = {totalTodos}
-        completed = {completedTodos}
-        />
-        <TodoSearch 
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-        />
+    <React.Fragment>
+        <TodoCounter />
+        <TodoSearch />
         <TodoList>
-        {searchedTodos.map(todo => (
-            <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}/>
-        ))}
+            {error && <p>Hubo un error...</p>}
+            {loading && <p>Cargando...</p>}
+            {(!loading && !searchedTodos.length) && <p>Crea tu primer Todos!</p>}
+            {searchedTodos.map(todo => (
+                <TodoItem
+                key={todo.text}
+                text={todo.text}
+                completed={todo.completed}
+                onComplete={() => completeTodo(todo.text)}
+                onDelete={() => deleteTodo(todo.text)}/>
+                ))}
         </TodoList>
-        <CreateTodoButton />
+
+        {!!openModal && (
+            <Modal>
+                <p>{searchedTodos[0]?.text}</p>
+            </Modal>
+        )}
+
+        <CreateTodoButton>
+            setOpenModal={setOpenModal}
+        </CreateTodoButton>
     </React.Fragment>
     );
 }
